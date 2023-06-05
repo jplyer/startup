abb7aee811d672469c87df61000e832cb9f0bfd2
jplyer <jplyer@byu.edu>
2023-06-05 17:23:01 -0600

    added a site nave option to index.html added about.js to pull a quote added about.html to display quote updated songlist.js with calls to the backend updated index.js with api support

--- index.js
+++ index.js
@@ -13,3 +13,49 @@ app.use(express.static('public'));
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+//creates the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
+  // Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+//getsonglist
+apiRouter.get('/songList', (_req, res) => {
+  res.send(songList);
+});
+
+//Submit songList
+apiRouter.get('/songList', (_req, res) => {
+  song = updateSongList(req.body, songList);
+  res.send(songList);
+});
+
+//sumbmitNewSong
+apiRouter.get('/songList', (_req, res) => {
+  songList = addSong(req.body, );
+  res.send(song);
+});
+
+let songList = [];
+
+function updateSongList(newSong, songList) {
+  let found = false;
+  for (const [i, prevList] of songList.entries()) {
+    if (newSong.songList > prevList.songList) {
+      songList.splice(i, 0, newSong);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    songList.push(newSong);
+  }
+
+  return scores;
+}
--- public/songlist.js
+++ public/songlist.js
@@ -1,5 +1,27 @@
 
-    let songList = [];
+    //default list for testing
+    let songList = [
+        {name:"I am a Microwave -The Inertia King", url:"https://www.youtube.com/watch?v=5bCXzPyKqSc"},
+        {name:"American Moon - Sailship", url:"https://sailship.bandcamp.com/track/american-moon"}
+    ];
+
+    async function loadSongs () {
+        try {
+    const response = await fetch('/api/songList');
+    scores = await response.json();
+
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('songList', JSON.stringify(songList));
+        } catch {
+            const songsText = localStorage.getItem('songList');
+
+            if (songsText) {
+                songs = JSON.parse(songsText);
+            }
+        }
+
+        displaySongs(songsText);
+    }
 
 document.getElementById("submitSong").addEventListener("click", function addSong() {
     let songName = document.getElementById("songNameEntry").value;
@@ -14,20 +36,21 @@ document.getElementById("submitSong").addEventListener("click", function addSong
     alert("Song submitted");
 });
 
-//placeholder code until I use node.js
-// let songs = document.getElementById("songList");
+//placeholder code until I can use DB 
+//I want to display it on main after user authentication for the site owner
+let songs = document.getElementById("songList");
 
-// songs.innerHTML = "";
+songs.innerHTML = "";
 
-// songList.forEach(function(song) {
-//     let li = document.createElement("li");
-//     let a = document.createElement("a");
+ songList.forEach(function displaySongs(song) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
 
-//     a.href = song.url;
-//     a.textContent = song.name;
+   a.href = song.url;
+    a.textContent = song.name;
 
-//     li.appendChild(a);
+    li.appendChild(a);
 
-//     songs.appendChild(li);
+    songs.appendChild(li);
 
-// });
+ });
