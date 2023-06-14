faf153f8903414525781dc9a0c373e88493ad7a1
jplyer <jplyer@byu.edu>
2023-06-13 18:40:35 -0600

    switched back to a modified version of the simon login it worked better this time

--- public/login.js
+++ public/login.js
@@ -1,35 +1,46 @@
 async function loginUser() {
-  const userName = document.getElementById('userName').value;
-  const password = document.getElementById('password').value;
+  loginOrCreate(`/api/auth/login`);
+}
 
-  const userData = {
-    username: userName,
-    password: password
-  };
-  console.log(userData)
-  try {
-    const response = await fetch('/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    });
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
 
-  
-    if (response.ok) {
-      const result = await response.json();
-      console.log(result);
-    } else {
-      const errorData = await response.json();
-      const errorMessage = document.getElementById('msgError');
-      
-      displayPopup(errorMessage);
-    }
-  
-  } catch (error) {
-    console.error(error);
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    console.log('response ok')
+    
+  } else {
+    const body = await response.json();
+    console.log(`⚠ Error: ${body.msg}`);
   }
-};
+}
 
+function logout() {
+  localStorage.removeItem('userName');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
 
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
